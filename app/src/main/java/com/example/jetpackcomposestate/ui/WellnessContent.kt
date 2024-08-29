05d02c59a79904f2a8a1912ae08aeffc75772760
Courtlyn Boykin <courtlynb@google.com>
2024-08-29 14:55:39 -0400

    add 'clear water count button' and a condition to show a WellnessTaskItem

--- app/src/main/java/com/example/jetpackcomposestate/ui/WellnessContent.kt
+++ app/src/main/java/com/example/jetpackcomposestate/ui/WellnessContent.kt
@@ -1,8 +1,13 @@
 package com.example.jetpackcomposestate.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableIntState
@@ -12,6 +17,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -23,18 +29,51 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by rememberSaveable { mutableIntStateOf(0) }
         if (count > 0) {
-            Text(
-                text = stringResource(R.string.you_ve_had_glasses, count),
-                modifier = modifier.padding(16.dp)
-            )
-        }
-        Button(
-            onClick = { count++ },
-            enabled = count < 10,
-            modifier = modifier.padding(top = 8.dp)) {
-            Text(stringResource(R.string.add_one))
+            var showTask by rememberSaveable { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = stringResource(R.string.walk),
+                    onClose = { showTask = false }
+                )
+            }
+                Text(
+                    text = stringResource(R.string.you_ve_had_glasses, count),
+                    modifier = modifier.padding(16.dp)
+                )
+            }
+            Row {
+                Button(
+                    onClick = { count++ },
+                    enabled = count < 10,
+                    modifier = modifier.padding(top = 8.dp)
+                ) {
+                    Text(stringResource(R.string.add_one))
+                }
+                Button(
+                    onClick = { count = 0 },
+                    modifier = modifier.padding(top = 8.dp)
+                ) {
+                    Text(stringResource(R.string.clear_water_count))
+                }
+            }
         }
     }
 
+@Composable
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically){
+        Text(modifier = Modifier
+            .weight(1f)
+            .padding(start = 16.dp),
+            text = taskName
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = stringResource(R.string.close))
 
+        }
+    }
 }
