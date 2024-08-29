cd5e3aa1ec9fd628c70b09b88c736abe878ddc35
Courtlyn Boykin <courtlynb@google.com>
2024-08-29 13:52:15 -0400

    update state management of count with rememberSaveable

--- app/src/main/java/com/example/jetpackcomposestate/ui/WellnessContent.kt
+++ app/src/main/java/com/example/jetpackcomposestate/ui/WellnessContent.kt
@@ -5,6 +5,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -12,8 +19,9 @@ import com.example.jetpackcomposestate.R
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+
     Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableIntStateOf(0) }
         Text(
             text = stringResource(R.string.you_ve_had_glasses, count),
             modifier = modifier.padding(16.dp)
